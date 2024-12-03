d73da12686e2ea174faa48730ae40fa639e497b5
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2024-12-03 16:34:01 +0100

    day 2 part 1

--- src/day02.zig
+++ src/day02.zig
@@ -11,7 +11,7 @@ const gpa = util.gpa;
 const data = @embedFile("data/day02.txt");
 
 pub fn main() !void {
-    
+    print("\n\npart1: {any}\n", .{part1(data)});
 }
 
 // Useful stdlib functions
@@ -44,3 +44,69 @@ const desc = std.sort.desc;
 // Generated from template/template.zig.
 // Run `zig build generate` to update.
 // Only unmodified days will be updated.
+fn part1(strLists: []const u8) !u64 {
+    var it = std.mem.tokenizeAny(u8, strLists, "\n");
+    var save_count: u64 = 0;
+    line_loop: while (it.next()) |line| {
+        print("\n", .{});
+        var numIt = tokenizeAny(u8, line, " ");
+        const first = try std.fmt.parseUnsigned(u64, numIt.next().?, 10);
+        const second = try std.fmt.parseUnsigned(u64, numIt.next().?, 10);
+        if (!diff_ok(first, second)) {
+            continue :line_loop;
+        }
+        var prev: u64 = second;
+        const up = first < second;
+        print("{s} ", .{if (up) "u" else "d"});
+        print("{d} {d} ", .{ first, second });
+        while (numIt.next()) |numStr| {
+            const num = try std.fmt.parseUnsigned(u64, numStr, 10);
+            print("{d} ", .{num});
+            if (prev < num) {
+                print("^", .{});
+                if (!up or !diff_ok(prev, num)) {
+                    print("invalidU: {d},{d}", .{ prev, num });
+                    continue :line_loop;
+                }
+            } else {
+                print("v", .{});
+                if (up or !diff_ok(prev, num)) {
+                    print("invalidD: {d},{d}", .{ prev, num });
+                    continue :line_loop;
+                }
+            }
+            prev = num;
+        }
+        print("✓", .{});
+        save_count += 1;
+    }
+
+    return save_count;
+}
+
+fn diff_ok(prev: u64, curr: u64) bool {
+    var diff: u64 = 0;
+    if (prev < curr) {
+        diff = curr - prev;
+    } else {
+        diff = prev - curr;
+    }
+
+    return diff >= 1 and diff <= 3;
+}
+
+const testInput =
+    \\7 6 4 2 1
+    \\1 2 7 8 9
+    \\9 7 6 2 1
+    \\1 3 2 4 5
+    \\8 6 4 4 1
+    \\1 3 6 7 9
+    \\1 3 2 1
+;
+test "part1 example" {
+    try std.testing.expectEqual(2, part1(testInput));
+}
+test "part2 example" {
+    //   try std.testing.expectEqual(31, part2(testInput));
+}
