1b2bf33fceed4168ca93ffc8e6de447493ca71c5
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2024-12-12 13:53:50 +0100

    day 10 part1

--- src/day10.zig
+++ src/day10.zig
@@ -11,7 +11,7 @@ const gpa = util.gpa;
 const data = @embedFile("data/day10.txt");
 
 pub fn main() !void {
-    
+    print("part1: {any}\n", .{scoreMap(data)});
 }
 
 // Useful stdlib functions
@@ -44,3 +44,100 @@ const desc = std.sort.desc;
 // Generated from template/template.zig.
 // Run `zig build generate` to update.
 // Only unmodified days will be updated.
+
+fn scoreMap(mapStr: []const u8) !u64 {
+    var map = List(u8).init(gpa);
+    defer map.deinit();
+    var it = tokenizeSca(u8, mapStr, '\n');
+    const width = it.peek().?.len;
+    while (it.next()) |line| {
+        try map.appendSlice(line);
+    }
+
+    const score = try gpa.alloc(u64, map.items.len);
+    defer gpa.free(score);
+    for (score, 0..) |_, ri| {
+        score[ri] = 0;
+    }
+
+    for (map.items, 0..) |h, i| {
+        if (h == '9') {
+            var reachables = try gpa.alloc(bool, map.items.len);
+            defer gpa.free(reachables);
+            for (reachables, 0..) |_, ri| {
+                reachables[ri] = false;
+            }
+            markReachable(map.items, &reachables, i, width);
+
+            for (reachables, 0..) |r, ri| {
+                if (r) score[ri] += 1;
+            }
+        }
+    }
+
+    var totalScore: u64 = 0;
+    for (map.items, 0..) |h, hi| {
+        if (@mod(hi, width) == 0) print("\n", .{});
+        print("[{c}:{any}]", .{ h, score[hi] });
+        if (h == '0') totalScore += score[hi];
+    }
+    return totalScore;
+}
+
+fn markReachable(map: []const u8, reachable: *[]bool, start: usize, width: usize) void {
+    // hook
+    if (reachable.*[start]) return else reachable.*[start] = true;
+
+    // >
+    var next = start + 1;
+    if (@mod(next, width) != 0) {
+        if (map[start] - 1 == map[next])
+            markReachable(map, reachable, next, width);
+    }
+    // v
+    next = start + width;
+    if (next < map.len) {
+        if (map[start] - 1 == map[next])
+            markReachable(map, reachable, next, width);
+    }
+    // <
+    if (start >= 1 and @mod(start, width) != 0) {
+        next = start - 1;
+        if (map[start] - 1 == map[next])
+            markReachable(map, reachable, next, width);
+    }
+    // ^
+    if (start >= width) {
+        next = start - width;
+        if (map[start] - 1 == map[next])
+            markReachable(map, reachable, next, width);
+    }
+}
+
+const exampleMap =
+    \\89010123
+    \\78121874
+    \\87430965
+    \\96549874
+    \\45678903
+    \\32019012
+    \\01329801
+    \\10456732
+;
+
+test "part1 example" {
+    try std.testing.expectEqual(36, scoreMap(exampleMap));
+}
+
+test "split example" {
+    const split =
+        \\...0...
+        \\...1...
+        \\...2...
+        \\6543456
+        \\7.....7
+        \\8.....8
+        \\9.....9
+    ;
+    try std.testing.expectEqual(2, scoreMap(split));
+}
