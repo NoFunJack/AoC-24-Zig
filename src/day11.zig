36b6ccc6aed5381d68d846ca029ab633fed480a4
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2024-12-13 13:45:19 +0100

    day 11 part 1

--- src/day11.zig
+++ src/day11.zig
@@ -11,7 +11,7 @@ const gpa = util.gpa;
 const data = @embedFile("data/day11.txt");
 
 pub fn main() !void {
-    
+    print("part1: {any}\n", .{countStones(data, 25)});
 }
 
 // Useful stdlib functions
@@ -44,3 +44,100 @@ const desc = std.sort.desc;
 // Generated from template/template.zig.
 // Run `zig build generate` to update.
 // Only unmodified days will be updated.
+
+fn countStones(input: []const u8, blinks: u16) !u64 {
+    var map = Map(u64, u64).init(gpa);
+
+    {
+        var itLine = tokenizeSca(u8, input, '\n');
+        var it = tokenizeSca(u8, itLine.next().?, ' ');
+        while (it.next()) |numStr| {
+            if (util.readUnsign(numStr)) |num| {
+                if (map.contains(num)) {
+                    const val = map.getPtr(num).?;
+                    val.* += 1;
+                } else {
+                    try map.put(num, 1);
+                }
+            } else |err| {
+                print("{any}", .{err});
+            }
+        }
+    }
+    printMap(map);
+    var currentBlinks = blinks;
+    while (currentBlinks > 0) {
+        currentBlinks -= 1;
+        print("\n\n**Blinks left: {d}\n", .{currentBlinks});
+        var nextMap = Map(u64, u64).init(gpa);
+        var it = map.iterator();
+        while (it.next()) |kv| {
+            const k = kv.key_ptr.*;
+            const v = kv.value_ptr.*;
+            // print("kv: {d},{d}\n", .{ k, v });
+            if (k == 0) {
+                try addEntry(&nextMap, 1, v);
+            } else if (@mod(std.math.log10_int(k) + 1, 2) == 0) {
+                const new = split(k);
+
+                for (new) |n| {
+                    try addEntry(&nextMap, n, v);
+                }
+            } else {
+                const mult = k * 2024;
+                // print("mult {d}->{d}\n", .{ k, mult });
+                try addEntry(&nextMap, mult, v);
+            }
+            //printMap(nextMap);
+        }
+        map.deinit();
+        // printMap(nextMap);
+        map = nextMap;
+    }
+
+    return sum(map);
+}
+
+fn sum(map: Map(u64, u64)) u64 {
+    var re: u64 = 0;
+    var vIt = map.valueIterator();
+    while (vIt.next()) |v| {
+        re += v.*;
+    }
+    return re;
+}
+
+fn split(num: u64) [2]u64 {
+    const len = (std.math.log10_int(num) + 1) / 2;
+
+    const pow = std.math.pow(u64, 10, len);
+    const r = num / pow;
+    const l = @mod(num, r * pow);
+    // print("split({d}):  {d}->{d},{d}\n", .{ len, num, r, l });
+    return .{ r, l };
+}
+
+fn addEntry(map: *Map(u64, u64), k: u64, v: u64) !void {
+    if (map.contains(k)) {
+        const p = map.getPtr(k).?;
+        p.* += v;
+    } else {
+        try map.put(k, v);
+    }
+}
+
+fn printMap(map: Map(u64, u64)) void {
+    print("---\n", .{});
+    var it = map.iterator();
+    while (it.next()) |kv| {
+        print("{d}: {d}\n", .{ kv.key_ptr.*, kv.value_ptr.* });
+    }
+    print("sum: {d}\n", .{sum(map)});
+    print("---\n", .{});
+}
+
+const exampleInput = "125 17";
+
+test "part1 example" {
+    try std.testing.expectEqual(55312, countStones(exampleInput, 25));
+}
