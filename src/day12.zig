745d9f200be03eb6af4bbab807e8b51dcf444bc7
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2024-12-14 12:26:23 +0100

    day 12 part 1small example done

--- src/day12.zig
+++ src/day12.zig
@@ -10,9 +10,7 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day12.txt");
 
-pub fn main() !void {
-    
-}
+pub fn main() !void {}
 
 // Useful stdlib functions
 const tokenizeAny = std.mem.tokenizeAny;
@@ -44,3 +42,128 @@ const desc = std.sort.desc;
 // Generated from template/template.zig.
 // Run `zig build generate` to update.
 // Only unmodified days will be updated.
+
+fn calcCost(mapStr: []const u8) !u64 {
+    var mapList = List(u8).init(gpa);
+    var lineIt = splitSca(u8, mapStr, '\n');
+    const width = lineIt.peek().?.len;
+    while (lineIt.next()) |line| {
+        try mapList.appendSlice(line);
+    }
+    const map = mapList.items;
+
+    var pcl = List(PlotCluster).init(gpa);
+    defer pcl.deinit();
+    var sum: u64 = 0;
+    plotLoop: for (map, 0..) |p, i| {
+        for (pcl.items) |pc| {
+            if (pc.isInPlot(p, i)) continue :plotLoop;
+        }
+        print("---New Plot {c} at {d}\n", .{ p, i });
+
+        const plot = try PlotCluster.init(width, p, i, &map);
+        try pcl.append(plot);
+        sum += plot.getCost();
+    }
+
+    return sum;
+}
+
+const PlotCluster = struct {
+    plots: []Plot,
+    width: usize,
+    letter: u8,
+    map: *const []u8,
+
+    pub fn init(width: usize, letter: u8, start: usize, map: *const []u8) !PlotCluster {
+        const plots = try gpa.alloc(Plot, map.len);
+        for (plots, 0..) |_, i| {
+            plots[i] = .{};
+        }
+
+        var re = PlotCluster{ .plots = plots, .width = width, .letter = letter, .map = map };
+        re.build(start);
+        return re;
+    }
+
+    fn build(self: *PlotCluster, start: usize) void {
+        self.buildInternal(start);
+        self.printMe();
+    }
+
+    fn buildInternal(self: *PlotCluster, pos: usize) void {
+        const pp = &self.plots[pos];
+        pp.*.exists = true;
+
+        // >
+        var next = pos + 1;
+        if (@mod(next, self.width) != 0 and pp.*.hasR and self.map.*[next] == self.letter) {
+            pp.*.hasR = false;
+            self.plots[next].hasL = false;
+            self.buildInternal(next);
+        }
+        // v
+        next = pos + self.width;
+        if (next < self.map.len and pp.*.hasD and self.map.*[next] == self.letter) {
+            pp.*.hasD = false;
+            self.plots[next].hasU = false;
+            self.buildInternal(next);
+        }
+    }
+
+    fn printMe(self: *PlotCluster) void {
+        for (self.plots, 0..) |p, i| {
+            if (@mod(i, self.width) == 0) print("\n", .{});
+            if (p.exists) {
+                print("{c}", .{self.letter});
+            } else {
+                print(".", .{});
+            }
+        }
+        print("\n", .{});
+    }
+
+    pub fn isInPlot(self: *const PlotCluster, letter: u8, pos: usize) bool {
+        return letter == self.letter and self.plots[pos].exists;
+    }
+
+    pub fn getCost(self: *const PlotCluster) u64 {
+        var area: u64 = 0;
+        var fence: u64 = 0;
+        for (self.plots) |p| {
+            if (p.exists) {
+                fence += p.fences();
+                area += 1;
+            }
+        }
+        const re = area * fence;
+        print("Result Plot[{c}] {d} * {d} = {d}\n", .{ self.letter, area, fence, re });
+        return re;
+    }
+};
+
+const Plot = struct {
+    exists: bool = false,
+    hasU: bool = true,
+    hasL: bool = true,
+    hasD: bool = true,
+    hasR: bool = true,
+
+    pub fn fences(self: *const Plot) u64 {
+        return @as(u64, @intFromBool(self.hasU)) +
+            @intFromBool(self.hasL) +
+            @intFromBool(self.hasD) +
+            @intFromBool(self.hasR);
+    }
+};
+
+const smallMap =
+    \\AAAA
+    \\BBCD
+    \\BBCC
+    \\EEEC
+;
+
+test "part1 small" {
+    try std.testing.expectEqual(140, calcCost(smallMap));
+}
