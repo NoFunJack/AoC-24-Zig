ae6fed56a80a9a013a3ec40dccbfb6c1d80437de
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2024-12-15 12:10:04 +0100

    day 13

--- src/day13.zig
+++ src/day13.zig
@@ -11,7 +11,8 @@ const gpa = util.gpa;
 const data = @embedFile("data/day13.txt");
 
 pub fn main() !void {
-    
+    print("total tickets: {any}\n", .{sumCheapMashines(data, 0)});
+    print("total tickets plus: {any}\n", .{sumCheapMashines(data, 10000000000000)});
 }
 
 // Useful stdlib functions
@@ -44,3 +45,72 @@ const desc = std.sort.desc;
 // Generated from template/template.zig.
 // Run `zig build generate` to update.
 // Only unmodified days will be updated.
+
+fn sumCheapMashines(dataStr: []const u8, plus: i64) !i64 {
+    var lineIt = tokenizeSca(u8, dataStr, '\n');
+    var sum: i64 = 0;
+    while (lineIt.next()) |buttonALine| {
+        const ba = try readButtonLine(buttonALine);
+        const bb = try readButtonLine(lineIt.next().?);
+        var r = try readResultLine(lineIt.next().?);
+        r[0] += plus;
+        r[1] += plus;
+
+        const num_y = (ba[1] * r[0]) - (ba[0] * r[1]);
+        const div_y = (bb[0] * ba[1]) - (ba[0] * bb[1]);
+        print("{d}/{d}=", .{ num_y, div_y });
+        if (std.math.divExact(i64, num_y, div_y)) |y| {
+            print("{d}\n", .{y});
+            if (std.math.divExact(i64, r[0] - (y * bb[0]), ba[0])) |x| {
+                print("-> x={d}\n", .{x});
+                const cost = (y + (3 * x));
+                print("-> cost={d}\n", .{cost});
+                sum += cost;
+            } else |err| {
+                print("divX: {any}\n", .{err});
+            }
+        } else |err| {
+            print("divY: {any}\n", .{err});
+        }
+    }
+    return sum;
+}
+
+fn readButtonLine(str: []const u8) ![2]i64 {
+    const xstr = str[12..14];
+    const ystr = str[18..20];
+    return .{
+        try parseInt(i64, xstr, 10),
+        try parseInt(i64, ystr, 10),
+    };
+}
+fn readResultLine(str: []const u8) ![2]i64 {
+    const xstr = str[indexOf(u8, str, 'X').? + 2 .. indexOf(u8, str, ',').?];
+    const ystr = str[indexOf(u8, str, 'Y').? + 2 ..];
+    return .{
+        try parseInt(i64, xstr, 10),
+        try parseInt(i64, ystr, 10),
+    };
+}
+
+const example =
+    \\Button A: X+94, Y+34
+    \\Button B: X+22, Y+67
+    \\Prize: X=8400, Y=5400
+    \\
+    \\Button A: X+26, Y+66
+    \\Button B: X+67, Y+21
+    \\Prize: X=12748, Y=12176
+    \\
+    \\Button A: X+17, Y+86
+    \\Button B: X+84, Y+37
+    \\Prize: X=7870, Y=6450
+    \\
+    \\Button A: X+69, Y+23
+    \\Button B: X+27, Y+71
+    \\Prize: X=18641, Y=10279
+;
+
+test "part1 example" {
+    try std.testing.expectEqual(480, sumCheapMashines(example, 0));
+}
