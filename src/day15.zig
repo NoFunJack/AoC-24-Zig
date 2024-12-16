d98beb723dacd4d5abff8be21fc6691f36297038
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2024-12-16 12:50:56 +0100

    day 15 part1

--- src/day15.zig
+++ src/day15.zig
@@ -11,7 +11,7 @@ const gpa = util.gpa;
 const data = @embedFile("data/day15.txt");
 
 pub fn main() !void {
-    
+    print("part1: {any}\n", .{boxHash(data)});
 }
 
 // Useful stdlib functions
@@ -44,3 +44,131 @@ const desc = std.sort.desc;
 // Generated from template/template.zig.
 // Run `zig build generate` to update.
 // Only unmodified days will be updated.
+
+fn boxHash(instr: []const u8) !u64 {
+    var blockIter = splitSeq(u8, instr, "\n\n");
+
+    // load map
+    var map = List(u8).init(gpa);
+    defer map.deinit();
+    var lineIt = splitSca(u8, blockIter.next().?, '\n');
+    const width = lineIt.peek().?.len;
+    {
+        while (lineIt.next()) |line| {
+            try map.appendSlice(line);
+        }
+    }
+    var pos = indexOf(u8, map.items, '@').?;
+    map.items[pos] = '.';
+    printMap(map.items, width, pos);
+
+    // move stuff
+    {
+        const moves = blockIter.next().?;
+        const iwidth: i8 = @intCast(width);
+        for (moves) |move| {
+            print("move: {c}\n", .{move});
+            switch (move) {
+                '<' => pos = changeMap(&map.items, width, pos, -1),
+                '^' => pos = changeMap(&map.items, width, pos, -iwidth),
+                '>' => pos = changeMap(&map.items, width, pos, 1),
+                'v' => pos = changeMap(&map.items, width, pos, iwidth),
+                else => {},
+            }
+        }
+    }
+
+    printMap(map.items, width, pos);
+    return calcHash(&map.items, width);
+}
+
+fn changeMap(map: *[]u8, width: usize, pos: usize, dir: i8) usize {
+    _ = width;
+    const next = pos +% @as(usize, @bitCast(@as(isize, dir)));
+    switch (map.*[next]) {
+        '#' => return pos,
+        '.' => return next,
+        'O' => {
+            var s = next;
+            while (map.*[s] != '#') {
+                s = s +% @as(usize, @bitCast(@as(isize, dir)));
+                if (map.*[s] == '.') {
+                    std.mem.swap(u8, &map.*[s], &map.*[next]);
+                    return next;
+                }
+            }
+            return pos;
+        },
+        else => unreachable,
+    }
+}
+
+fn calcHash(map: *[]u8, width: usize) u64 {
+    var sum: u64 = 0;
+    for (map.*, 0..) |m, i| {
+        if (m == 'O') {
+            const l = @rem(i, width);
+            const t = @divTrunc(i, width);
+            sum += t * 100 + l;
+        }
+    }
+    return sum;
+}
+
+fn printMap(map: []u8, width: usize, pos: usize) void {
+    for (map, 0..) |m, i| {
+        if (i == pos) {
+            print("@", .{});
+        } else {
+            if (@mod(i, width) == 0) {
+                print("\n", .{});
+            }
+            print("{c}", .{m});
+        }
+    }
+    print("\n\n", .{});
+}
+
+const example =
+    \\########
+    \\#..O.O.#
+    \\##@.O..#
+    \\#...O..#
+    \\#.#.O..#
+    \\#...O..#
+    \\#......#
+    \\########
+    \\
+    \\<^^>>>vv<v>>v<<
+;
+
+test "part1 example small" {
+    try std.testing.expectEqual(2028, boxHash(example));
+}
+const exampleBig =
+    \\##########
+    \\#..O..O.O#
+    \\#......O.#
+    \\#.OO..O.O#
+    \\#..O@..O.#
+    \\#O#..O...#
+    \\#O..O..O.#
+    \\#.OO.O.OO#
+    \\#....O...#
+    \\##########
+    \\
+    \\<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+    \\vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+    \\><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+    \\<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+    \\^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+    \\^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+    \\>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+    \\<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+    \\^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+    \\v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+;
+
+test "part1 example big" {
+    try std.testing.expectEqual(10092, boxHash(exampleBig));
+}
